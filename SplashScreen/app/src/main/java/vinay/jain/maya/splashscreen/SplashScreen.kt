773a5739097c4b0cc4ba15b84bb4ead5e55c356e
VinayMJ <vinayjainmaya@gmail.com>
2018-11-15 22:34:11 +0530

    splash screen changes

--- SplashScreen/app/src/main/java/vinay/jain/maya/splashscreen/SplashScreen.kt
+++ SplashScreen/app/src/main/java/vinay/jain/maya/splashscreen/SplashScreen.kt
@@ -8,20 +8,27 @@ import android.support.v7.app.AppCompatActivity;
 
 class SplashScreen : AppCompatActivity() {
 
+    private var mHandler: Handler? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
 
-        var mHandler: Handler = Handler()
-        var mRunnable: Runnable = Runnable {
+        var mHandler = Handler()
 
-            var i: Intent = Intent(this@SplashScreen, MainActivity::class.java)
-            startActivity(i)
-            finish()
-        }
-
-        mHandler.postDelayed(mRunnable,5000)
+        mHandler!!.postDelayed(mRunnable,3000)
 
     }
 
+    var mRunnable: Runnable = Runnable {
+
+        var i: Intent = Intent(this@SplashScreen, MainActivity::class.java)
+        startActivity(i)
+        finish()
+    }
+
+    override fun onDestroy() {
+        mHandler?.removeCallbacks(mRunnable)
+        super.onDestroy()
+    }
 }
